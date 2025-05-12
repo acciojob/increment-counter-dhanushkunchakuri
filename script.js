f2f1b104b0cc2cec55b0ade91d0da16587e74f7b
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-12 16:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 let counter=0;
-let main=document.getElementById("incrementBtn");
-let ele=document.getElementById("counter");
-
-
 function incrementbtn() {
 	alert(counter);
 	counter+=1;
 	ele.textContent=counter;
 }
+let main=document.getElementById("incrementBtn");
+let ele=document.getElementById("counter");
+
+
