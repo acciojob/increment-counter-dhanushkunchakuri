07f026ec570db0d50abe47c5b33d1bca0a210089
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-12 16:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let counter=0;
+// let main=document.getElementById("body");
+let ele=document.getElementById("demo");
+
+
+function incrementbtn() {
+	alert(counter);
+	counter+=1;
+	ele.textContent=counter;
+}
